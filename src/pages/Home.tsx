ca1ae02bdd498cd6ba71ec52913fabbd35c2ddf8
JAENY <wjswodus22@naver.com>
2022-08-22 03:12:12 +0900

    master: 불필요 코드 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import BackToTopArrow from 'app.components/BackToTopArrow';
 import ScreenHwaSomBoard from 'app.feature/board/screen/ScreenHwaSomBoard';
-import styled from 'styled-components';
 
 const Home = () => {
   return (
